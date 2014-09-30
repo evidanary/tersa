a6ef640ac9a5d92e3174a66bad2800192075357c
Yash Ranadive <yash.ranadive@gmail.com>
2014-09-29 22:09:21 -0700

    Create Grid and Create Entity working

--- js/include.js
+++ js/include.js
@@ -6,9 +6,11 @@
 //"blocks" in a variable name reprents the number of blocks
 
 //Returns a grid mesh that can be added to the scene
-function createGrid(blocksX, blocksZ) {
+//Grid mesh is located in +x, +y, +z quadrant
+function createGrid(blocksRowsColumns) {
     //Creates grid of a particular dimension measure in number of voxel block	
     var step = 50;
+    var blocksX = blocksRowsColumns, blocksZ = blocksRowsColumns;
     var sizeX = blocksX * step;
     var sizeZ = blocksZ * step;
 
@@ -16,7 +18,7 @@ function createGrid(blocksX, blocksZ) {
 
     for ( var i = 0; i <= sizeX; i += step ) {
 	geometry.vertices.push( new THREE.Vector3( 0, 0, i ) );
-	geometry.vertices.push( new THREE.Vector3(   sizeX, 0, i ) );
+	geometry.vertices.push( new THREE.Vector3( sizeX, 0, i ) );
     }
 
     for ( var i = 0; i <= sizeZ; i += step ) {
@@ -29,12 +31,26 @@ function createGrid(blocksX, blocksZ) {
     line.type = THREE.LinePieces;
 
     return line;
-
 }
 
-
-function createEntity(blocksX, blocksY, locationX, locationY, name) {
+//Returns a cube mesh at the given location
+function createEntity(blocksX, blocksY, locationX, locationY, locationZ, name) {
     //Adds an entity to the grid
+    var cubeGeo, cubeMaterial;
+    var voxelPosition = new THREE.Vector3();
+    cubeGeo = new THREE.BoxGeometry( 50, 50, 50 );
+    cubeMaterial = new THREE.MeshLambertMaterial( { color: 0xfeb74c, ambient: 0x00ff80 } );
+    cubeMaterial.ambient = cubeMaterial.color;
+
+    var voxel = new THREE.Mesh( cubeGeo, cubeMaterial );
+    voxel.position.x = locationX * 50 + 25;
+    voxel.position.y = locationY * 50 + 25;
+    voxel.position.z = locationZ * 50 + 25;
+    voxel.matrixAutoUpdate = false;
+    voxel.updateMatrix();
+    
+    objects.push( voxel );
+    return voxel;
 }
 
 function createPipe(blockX, blockY, name, fromEntity, toEntity, length) {
